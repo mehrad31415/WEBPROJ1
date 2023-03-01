864b27617ec4c40fea9e583897072f28a1e30601
ErikRiel <epmriel@gmail.com>
2023-03-01 16:55:31 +0100

    start with js

---
+++ scripts/myScript.js
@@ -0,0 +1,31 @@
+//Javascript
+
+//classes for info-page
+    //movie (3: Erik)
+
+
+    //artist (1: Mehrad)
+
+
+        //director
+
+
+        //writer 
+
+
+        //actor
+
+
+//menus (2: Lars)
+    //header menu
+    
+    // all elements in the menu 
+
+    const elementNames = ["body", "header", "footeer", "aside", "articles", "section"]; 
+
+
+    //footer menu
+
+   
+
+//tooltip (3: Erik)
