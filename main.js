1535fb44ac65b651f66a11071082773b3c4f9418
mehrad31415 <m.haghshenas@students.uu.nl>
2023-03-24 15:56:11 +0100

    node

---
+++ main.js
@@ -0,0 +1,14 @@
+#!/usr/bin nodejs
+const http = require('node:http');
+const fs   = require('node:fs');
+const express = require ('express');
+
+const index = fs.readFileSync('html/index.html','utf-8');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(index);
+    res.end();
+});
+
+server.listen(5001,'localhost');
+
