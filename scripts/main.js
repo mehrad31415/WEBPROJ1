c68738a915b2468a84ddf414dbeca7dd126a50e8
mehrad31415 <m.haghshenas@students.uu.nl>
2023-03-24 15:55:38 +0100

    node

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin nodejs
-
-const http = require('http');
-
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World Test 2.');
-    res.end();
-}).listen(8015,'localhost');
